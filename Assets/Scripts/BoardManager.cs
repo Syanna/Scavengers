e837e0a3529a7a2d955d46f016d2eba5b430af1c
syanna <syanna@users.noreply.github.com>
2015-06-12 18:09:32 -0700

    Initial setup of prefabs

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max){
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count(5,9);
+	public Count foodCount = new Count(1,5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outerWallTiles;
+
+	private Transform boardHolder;
+	private List<Vector3> 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
